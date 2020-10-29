dce9df80a18680c2fbc5d7dd63bbae0b6d11da9a
vaishnavi9247263609 <54807556+vaishnavi9247263609@users.noreply.github.com>
2020-10-29 14:50:48 +0530

    Add files via upload

---
+++ assignment2/table.js
@@ -0,0 +1,20 @@
+function insert_Row()
+{
+var x=document.getElementById('table').insertRow(0);
+var y = x.insertCell(0);
+var z = x.insertCell(1);
+var a = x.insertCell(2);
+var b = x.insertCell(3);
+var c = x.insertCell(4);
+var d = x.insertCell(5);
+var e = x.insertCell(6);
+var f = x.insertCell(7);
+var g = x.insertCell(8);
+var h = x.insertCell(9);
+var i = x.insertCell(10);
+var j = x.insertCell(11);
+var k = x.insertCell(12);
+y.innerHTML="New Cell1";
+z.innerHTML="New Cell2";
+
+}
